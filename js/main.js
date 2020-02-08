f3c32871b92592df15547a396d01754d135c0048
Skylar Wong <skylar@Skylars-MacBook-Air.local>
2020-02-08 16:20:27 -0600

    start genBombs func

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ const td = document.querySelector('td');
 
 /*----- app's state (variables) -----*/
 let cellIDArray = []; //create array to store cell id.. ---> do i even need this?
+let numRows = 10; //can be user input in future
+let numCols = 10;
 
 /*----- cached element references -----*/
 
@@ -13,10 +15,10 @@ let cellIDArray = []; //create array to store cell id.. ---> do i even need this
 /*----- functions -----*/
 
 function initialRender(){
-    for (let i=0; i<10; i++){
+    for (let i=0; i<numRows; i++){
         cellIDArray[i] = new Array(); //create new array within array (row array)
         var row = table.insertRow(i);
-        for (let j=0; j<10; j++){
+        for (let j=0; j<numCols; j++){
             var cell = row.insertCell(-1); //accepts -1 or 0. -1 is to the left
             //cell.innerHTML = `${i}${j}`; //this is just to show the id of each cell so i know what it is
             cell.id=`${i}${j}`; //store id of cell -- should i put this in an array?
@@ -25,13 +27,30 @@ function initialRender(){
     }
 }
 
-function rndIdxI(){
-    return idx = Math.floor(Math.random()*10);
+function rndIdx(num){  //pass in num for either numRows or numCols that we want to multiply by
+    return idx = Math.floor(Math.random()*num);
 }
+
+function genBombs(num){ //pass in how many bombs to generate
+    let bombArr =[];
+    for (let i=0; i<num; i++){
+        let rowIdx = rndIdx(numRows);
+        let colIdx = rndIdx(numCols);
+        bombIndex = `${rowIdx}${colIdx}`;
+        bombArr.push(bombIndex);
+    }
+    //return bombArr;
+
+}
+
+
+
+
 
 
 initialRender();
 console.log(cellIDArray);
 // cell02 = document.getElementById('02');
+// cell02.style.backgroundColor = "red";
 // cell02.textContent = 'blah';
 
