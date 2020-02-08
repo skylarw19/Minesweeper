1482a89cc82117674193dea778d812f756dcdf9a
Skylar Wong <skylar@Skylars-MacBook-Air.local>
2020-02-08 15:40:39 -0600

    create working 2D array

--- js/main.js
+++ js/main.js
@@ -10,19 +10,27 @@ let cellID = []; //create array to store cell id..
 /*----- event listeners -----*/
 /*----- functions -----*/
 
-function init(){
-    var x = document.createElement("td")
-    tr.appendChild(x);
-}
+// function render(){
+//     for (let i=0; i<10; i++){
+//         var row = table.insertRow(i);
+//         for (let j=0; j<10; j++){
+//             var cell = row.insertCell(-1); //accepts -1 or 0. -1 is to the left
+//             cell.innerHTML = `${i}${j}`;
+//             cell.id=`${i}${j}`; //store id of cell -- should i put this in an array?
+//             cellID.push(cell.id);
+//         }
+//     }
+// }
 
 function render(){
     for (let i=0; i<10; i++){
+        cellID[i] = new Array(); //create new array within array (row array)
         var row = table.insertRow(i);
         for (let j=0; j<10; j++){
             var cell = row.insertCell(-1); //accepts -1 or 0. -1 is to the left
             cell.innerHTML = `${i}${j}`;
-            cell.id=`${i}${j}`; //store id of cell -- should i put this in an array
-            cellID[i].push(cell.id);
+            cell.id=`${i}${j}`; //store id of cell -- should i put this in an array?
+            cellID[i].push(cell.id); //push to end of current row array (i)
         }
     }
 }
@@ -30,3 +38,6 @@ function render(){
 
 render();
 console.log(cellID);
+// cell02 = document.getElementById('02');
+// cell02.textContent = 'blah';
+
