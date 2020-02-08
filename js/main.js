951d7667d0384205af85ac8e503c7949c4ef13c6
Skylar Wong <skylar@Skylars-MacBook-Air.local>
2020-02-08 15:58:21 -0600

    update initial render

--- js/main.js
+++ js/main.js
@@ -4,40 +4,34 @@ const tr = document.querySelector('tr');
 const td = document.querySelector('td');
 
 /*----- app's state (variables) -----*/
-let cellID = []; //create array to store cell id.. 
+let cellIDArray = []; //create array to store cell id.. ---> do i even need this?
 
 /*----- cached element references -----*/
+
+
 /*----- event listeners -----*/
 /*----- functions -----*/
 
-// function render(){
-//     for (let i=0; i<10; i++){
-//         var row = table.insertRow(i);
-//         for (let j=0; j<10; j++){
-//             var cell = row.insertCell(-1); //accepts -1 or 0. -1 is to the left
-//             cell.innerHTML = `${i}${j}`;
-//             cell.id=`${i}${j}`; //store id of cell -- should i put this in an array?
-//             cellID.push(cell.id);
-//         }
-//     }
-// }
-
-function render(){
+function initialRender(){
     for (let i=0; i<10; i++){
-        cellID[i] = new Array(); //create new array within array (row array)
+        cellIDArray[i] = new Array(); //create new array within array (row array)
         var row = table.insertRow(i);
         for (let j=0; j<10; j++){
             var cell = row.insertCell(-1); //accepts -1 or 0. -1 is to the left
-            cell.innerHTML = `${i}${j}`;
+            //cell.innerHTML = `${i}${j}`; //this is just to show the id of each cell so i know what it is
             cell.id=`${i}${j}`; //store id of cell -- should i put this in an array?
-            cellID[i].push(cell.id); //push to end of current row array (i)
+            cellIDArray[i].push(cell.id); //push to end of current row array (i)
         }
     }
 }
 
+function rndIdxI(){
+    return idx = Math.floor(Math.random()*10);
+}
 
-render();
-console.log(cellID);
+
+initialRender();
+console.log(cellIDArray);
 // cell02 = document.getElementById('02');
 // cell02.textContent = 'blah';
 
