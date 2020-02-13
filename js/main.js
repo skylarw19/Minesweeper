26dbae93787973e7b5f1aa8b1c809b7f5ecdd93f
Skylar Wong <skylar@Skylars-Air.lan>
2020-02-12 22:18:56 -0600

    update comments

--- js/main.js
+++ js/main.js
@@ -23,15 +23,15 @@ restart.addEventListener('click',init);
 function reveal(evt){
         let cellID = evt.target.id;
         if(isGameOver === false){
-            if (playerArray[cellID] === null || playerArray[cellID] === "flag"){  
+            if (playerArray[cellID] === null || playerArray[cellID] === "flag"){ //if not yet revealed or flag
                 if (playerArray[cellID] === "flag")
                     numFlagsLeft++; 
                 playerArray[cellID] = boardArray[cellID];
             }  
-            if(boardArray[cellID]===0) //if it clicked on zero
-                search(cellID);
+            if(boardArray[cellID]===0) //if clicked zero, reveal all connecting zeros
+                searchZero(cellID);
         }
-        if(boardArray[cellID]==="bomb"){
+        if(boardArray[cellID]==="bomb"){ //if clicked bomb will need to reveal all bombs
             for(let i=0; i<boardArray.length; i++){
                 if (boardArray[i] === "bomb")
                     playerArray[i] = "bomb";   
@@ -53,10 +53,121 @@ function flag(evt){
     render();
 }
 
+function init(){
+    //clear board
+    table.innerHTML = "";
+    boardArray.splice(0,boardArray.length);
+
+    //generate board and boardArray
+    var numSquares =0;
+    for (let i=0; i<numRows; i++){
+        var row = table.insertRow(i);
+        for (let j=0; j<numCols; j++){
+            var cell = row.insertCell(-1);
+            cell.id=`${numSquares}`; //set id of td
+            boardArray.push(0); 
+            numSquares++;
+        }
+    }
+    //gen player array and change edges to "-"
+    for (let i=0; i<boardArray.length; i++){
+       if (i<numCols){ //top row
+           boardArray[i] = "-";
+           playerArray[i] = "-";
+       }
+       else if (i%numCols===0){ //left column
+            boardArray[i] = "-";
+            playerArray[i] = "-";
+       }
+       else if ((i+1)%numCols===0){ //right column
+            boardArray[i] = "-";
+            playerArray[i] = "-";
+       }
+       else if (i>(boardArray.length-numCols) && i<boardArray.length){ //bottom row
+           boardArray[i] = "-";
+           playerArray[i] = "-";
+       }
+       else playerArray[i] = null;
+    }
+    genBombs(numberBombs,0);
+    genNum();
+    isGameOver = false;
+    render();
+}
+
+function rndIdx(numBombs){  //pass in num for either numRows or numCols that we want to multiply by
+    return idx = Math.floor(Math.random()*numBombs);
+}
+
+function genBombs(numBombs, bombCount){
+    while (bombCount < numBombs){ //gen Bombs with no repeat indexes
+        let bombIndex = rndIdx(boardArray.length)
+        if(boardArray[bombIndex]!=="-" && boardArray[bombIndex] !== "bomb"){
+            boardArray[bombIndex] = "bomb"; 
+            bombCount++;
+        }
+    }
+    // ensure a bomb is not completely surrounded by other bombs
+    for (let i=0; i<boardArray.length; i++){
+        if(isBomb(i)){
+            let neighboringBombs = 0;
+            for (let j = 0; j<8; j++){
+                let location = [i-numCols-1,i-numCols,i-numCols+1,i+1,i+numCols+1,i+numCols,i+numCols-1,i-1];
+                if (isBomb(location[j]) || boardArray[location[j]] === "-")
+                    neighboringBombs++;
+            }
+            if (neighboringBombs === 8){
+                boardArray[i] = 0;
+                bombCount--;
+                genBombs(numBombs,bombCount);
+            }
+        }  
+    }
+    return boardArray;
+}
+
+function isBomb(i){
+    return boardArray[i]==="bomb";
+}
+
+function genNum(){
+    for (let i=0; i<boardArray.length; i++){
+        let location = [i-numCols-1,i-numCols,i-numCols+1,i+1,i+numCols+1,i+numCols,i+numCols-1,i-1]
+        if(boardArray[i]!=="bomb" && boardArray[i]!== "-"){ //if sq is not bomb and not border, count surroundings;
+            let count = 0;
+            for (let j=0; j<8; j++){
+                if (isBomb(location[j])) count++;
+            }
+            boardArray[i] = count;
+        }
+    }
+    return boardArray;
+}
+
+//if zero is clicked, all connecting zeros must be revealed until it reveals number
+function searchZero(i){
+    i = parseInt(i);
+    let location = [i-numCols-1,i-numCols,i-numCols+1,i+1,i+numCols+1,i+numCols,i+numCols-1,i-1];
+    for (let j=0; j<8; j++){
+        if (boardArray[location[j]] !== 0 && boardArray[location[j]] !== "bomb"){
+            if (playerArray[location[j]] === null){
+                playerArray[location[j]] = boardArray[location[j]];
+            } 
+        }
+        else if (boardArray[location[j]]===0){
+            if (playerArray[location[j]] === null){
+                playerArray[location[j]] = boardArray[location[j]];
+                searchZero(location[j]);
+            }
+        }
+    }
+    return playerArray;
+}
+
 function render(){
     for (let i=0; i<boardArray.length; i++){
         let cellEl = document.getElementById(i);
-        if (playerArray[i] !== "-"){
+        if (playerArray[i] !== "-"){  //if edge, do not show on board
             cellEl.classList.add("regular");
             if (playerArray[i] === "flag"){
                 cellEl.textContent =  "❄️"; 
@@ -69,7 +180,7 @@ function render(){
             else {
                 cellEl.textContent = playerArray[i];
                 if (playerArray[i]!== null)
-                    cellEl.classList.add("revealNum"); //--> show lighter color
+                    cellEl.classList.add("revealNum"); 
             }
         } else cellEl.classList.add("edge");
     }
@@ -94,120 +205,6 @@ function isWinner(numberBombs){
             flagCount++;
     }
     if (flagCount === numberBombs) return true;
-}
-
-function init(){
-    //clear board
-    table.innerHTML = "";
-    boardArray.splice(0,boardArray.length);
-
-    //init board
-    var numSquares =0;
-    for (let i=0; i<numRows; i++){
-        var row = table.insertRow(i);
-        for (let j=0; j<numCols; j++){
-            var cell = row.insertCell(-1);
-            cell.id=`${numSquares}`; //each td of the cell has an id matching an arrayindex
-            boardArray.push(0); //initialize board array with value of 0
-            numSquares++;
-        }
-    }
-    //gen player array
-    for (let i=0; i<boardArray.length; i++){
-        playerArray[i] = null;
-    }
-    //change edges to -
-    for (let i=0; i<boardArray.length; i++){
-       if (i<numCols){ //top row
-           boardArray[i] = "-";
-           playerArray[i] = "-";
-       }
-       if (i%numCols===0){ //left column
-            boardArray[i] = "-";
-            playerArray[i] = "-";
-       }
-       if ((i+1)%numCols===0){ //right column
-            boardArray[i] = "-";
-            playerArray[i] = "-";
-       }
-       if (i>(boardArray.length-numCols) && i<boardArray.length){ //bottom row
-           boardArray[i] = "-";
-           playerArray[i] = "-";
-       }
-    }
-    genBombs(numberBombs,0);
-    genNum();
-    isGameOver = false;
-    render();
-}
-
-function rndIdx(numBombs){  //pass in num for either numRows or numCols that we want to multiply by
-    return idx = Math.floor(Math.random()*numBombs);
-}
-
-function genBombs(numBombs, bombCount){
-    while (bombCount < numBombs){ //gen Bombs with no repeat indexes
-        let bombIndex = rndIdx(boardArray.length)
-        if(boardArray[bombIndex]!=="-" && boardArray[bombIndex] !== "bomb"){
-            boardArray[bombIndex] = "bomb"; 
-            bombCount++;
-        }
-    }
-    
-     // ensure a bomb is not completely surrounded by other bombs
-    for (let i=0; i<boardArray.length; i++){
-        if(boardArray[i] === "bomb"){
-            let neighboringBombs = 0;
-            for (let j = 0; j<8; j++){
-                let location = [i-numCols-1,i-numCols,i-numCols+1,i+1,i+numCols+1,i+numCols,i+numCols-1,i-1];
-                if (boardArray[location[j]] === "bomb" || boardArray[location[j]] === "-")
-                    neighboringBombs++;
-            }
-            if (neighboringBombs === 8){
-                boardArray[i] = 0;
-                bombCount--;
-                genBombs(numBombs,bombCount);
-            }
-        }  
-    }
-    return boardArray;
-}
-
-function isBomb(i){
-    return boardArray[i]==="bomb";
-}
-
-function genNum(){
-    for (let i=0; i<boardArray.length; i++){
-        let location = [i-numCols-1,i-numCols,i-numCols+1,i+1,i+numCols+1,i+numCols,i+numCols-1,i-1]
-        if(boardArray[i]!=="bomb" && boardArray[i]!== "-"){ //if sq not bomb and not border, count surroundings;
-            let count = 0;
-            for (let j=0; j<8; j++){
-                if (isBomb(location[j])) count++;
-            }
-            boardArray[i] = count;
-        }
-    }
-    return boardArray;
-}
-
-function search(i){
-    i = parseInt(i);
-    let location = [i-numCols-1,i-numCols,i-numCols+1,i+1,i+numCols+1,i+numCols,i+numCols-1,i-1];
-    for (let j=0; j<8; j++){
-        if (boardArray[location[j]] !== 0 && boardArray[location[j]] !== "bomb"){
-            if (playerArray[location[j]] === null){
-                playerArray[location[j]] = boardArray[location[j]];
-            } 
-        }
-        else if (boardArray[location[j]]===0){
-            if (playerArray[location[j]] === null){
-                playerArray[location[j]] = boardArray[location[j]];
-                search(location[j]);
-            }
-        }
-    }
-    return playerArray;
 }
 
 init();
